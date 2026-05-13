064bc1f4bb18016b87ddc3553553f4a19afabaf7
eknlau@gmail.com <eknlau@gmail.com>
2026-05-13 14:23:54 +0800

    update

--- global.py
+++ global.py
@@ -24,7 +24,7 @@ run_str = dt.strftime("%Y-%m-%d %H:%M")
 save_path = r"/Users/eknlau/VS_code/GHMWS-global-model/ECMWF/NWP/850hPa wind+MSLP" # Adjust for Linux if needed
 os.makedirs(save_path, exist_ok=True)
 
-fxx_list = [0,6,12,18,24,30,36,42, 48,54,60,66,72,78,84,90, 96,102,108,114, 120,126,132,138,144,150,156,162, 168,174,180, 192,204, 216,228, 240]
+fxx_list = [0,6,12,18,24,30,36,42, 48,54,60,66,72,84,96,108,120,144,168,192,216,240]
 
 # 2. Main Processing Loop
 for fxx in fxx_list:
